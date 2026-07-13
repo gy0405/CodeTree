ea266433e4168101175d4a03a934d93b34b75f1c
Nuyo <lgy000405@gmail.com>
2026-07-13 21:41:24 +0900

    [Passed] 변수 출력하기 2 (98ms, 11MB)

---
+++ trail1/변수 출력하기 2/outputing-variables-2.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        char a = 'C'; int b = 3;
+        System.out.printf("%c!.....!%d", a, b);
+    }
+}
