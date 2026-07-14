a5f05738c58590e31674d54023205350401a8e93
Nuyo <lgy000405@gmail.com>
2026-07-14 14:49:03 +0900

    [Passed] 문자열 순서 바꾸기 (128ms, 10MB)

---
+++ trail1/문자열 순서 바꾸기/change-order-of-strings.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        String S = sc.next();
+        String T = sc.next();
+
+        String temp = S;
+        S = T;
+        T = temp;
+
+        System.out.println(S);
+        System.out.println(T);
+
+    }
+}
