5d8e23c0d019143503d1d12f553fab8d6c890f2e
Nuyo <lgy000405@gmail.com>
2026-07-15 13:15:22 +0900

    [Passed] 점수 비교 (142ms, 10MB)

---
+++ trail1/점수 비교/score-comparison.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int am = sc.nextInt();
+        int ae = sc.nextInt();
+        int bm = sc.nextInt();
+        int be = sc.nextInt();
+
+        if (am > bm && ae > be){
+            System.out.println(1);
+        }
+        else{
+            System.out.println(0);
+        }
+    }
+}
