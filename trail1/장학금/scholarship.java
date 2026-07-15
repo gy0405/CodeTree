8161c2dae402fe2efa4aff3beb6215b98a3d667d
Nuyo <lgy000405@gmail.com>
2026-07-15 13:44:54 +0900

    [Passed] 장학금 (129ms, 10MB)

---
+++ trail1/장학금/scholarship.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int mid = sc.nextInt();
+        int fin = sc.nextInt();
+
+        if (mid < 90){
+            System.out.println(0);
+        }
+        else if (fin >= 95){
+            System.out.println(100000);
+        }
+        else if (fin >= 90){
+            System.out.println(50000);
+        }
+        else{
+            System.out.println(0);
+        }
+    }
+}
