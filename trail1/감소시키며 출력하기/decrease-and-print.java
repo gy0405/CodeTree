8d4f53625be1ff1144823f13d8bda99cfa560857
Nuyo <lgy000405@gmail.com>
2026-07-16 13:46:08 +0900

    [Passed] 감소시키며 출력하기 (117ms, 10MB)

---
+++ trail1/감소시키며 출력하기/decrease-and-print.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        
+        for (int i = 17; i > 4; i--){
+            System.out.print(i + " ");
+        }
+    }
+}
