a52a9867494c7f2d129c18f1f895c56c8d7d9c37
Nuyo <lgy000405@gmail.com>
2026-07-18 13:16:27 +0900

    [Passed] A 부터 B 까지 (111ms, 9MB)

---
+++ trail1/A 부터 B 까지/a-to-b.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] lines = br.readLine().split(" ");
+
+        int A = Integer.parseInt(lines[0]);
+        int B = Integer.parseInt(lines[1]);
+
+        while (A <= B){
+            System.out.print(A + " ");
+
+            if (A % 2 == 1){
+                A *= 2;
+            }
+            else {
+                A += 3;
+            }
+        }
+    }
+}
