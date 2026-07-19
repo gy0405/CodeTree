ede2ac82ce0315bd82259f8b6c4a62dadb7ebf6b
Nuyo <lgy000405@gmail.com>
2026-07-19 13:21:19 +0900

    [Passed] 5의 배수의 합 (98ms, 9MB)

---
+++ trail1/5의 배수의 합/sum-of-specific-condition-multiple.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] lines = br.readLine().split(" ");
+
+        int A = Integer.parseInt(lines[0]);
+        int B = Integer.parseInt(lines[1]);
+        int sum = 0;
+
+        if (A >= B){
+            for (int i = B; i < A+1; i++){
+                if (i % 5 == 0){
+                    sum += i;
+                }
+            }
+        }
+        else {
+            for (int i = A; i < B+1; i++){
+                if (i % 5 == 0){
+                    sum += i;
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+}
