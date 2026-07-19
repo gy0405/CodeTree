d1893f044856236e982d12f3a3d4512295008597
Nuyo <lgy000405@gmail.com>
2026-07-19 13:10:06 +0900

    [Passed] 100부터 입력받는 수까지의 합 (98ms, 9MB)

---
+++ trail1/100부터 입력받는 수까지의 합/sum-of-the-number-input-from-100.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+
+        for (int i = n; i < 101; i++){
+            sum += i;
+        }
+
+        System.out.println(sum);
+    }
+}
