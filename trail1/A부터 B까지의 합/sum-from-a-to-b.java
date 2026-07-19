dab0221bce9b6d2d053b77bcb175877def285de7
Nuyo <lgy000405@gmail.com>
2026-07-19 13:07:57 +0900

    [Passed] A부터 B까지의 합 (88ms, 9MB)

---
+++ trail1/A부터 B까지의 합/sum-from-a-to-b.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] lines = br.readLine().split(" ");
+        int A = Integer.parseInt(lines[0]);
+        int B = Integer.parseInt(lines[1]);
+
+        int sum = 0;
+
+        for (int i = A; i < B+1; i++){
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
