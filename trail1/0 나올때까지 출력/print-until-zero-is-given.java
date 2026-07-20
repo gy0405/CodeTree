1f648aade6bb57dbdf0dbf728f27f0b6219becb5
Nuyo <lgy000405@gmail.com>
2026-07-21 07:55:55 +0900

    [Passed] 0 나올때까지 출력 (87ms, 9MB)

---
+++ trail1/0 나올때까지 출력/print-until-zero-is-given.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while(true){
+            int x = Integer.parseInt(br.readLine());
+            if (x == 0){
+                break;
+            }
+            System.out.println(x);
+        }
+    }
+}
