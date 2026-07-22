c73ad2ec173a0efe2484e14f8b3f8e5c706c4567
Nuyo <lgy000405@gmail.com>
2026-07-22 13:22:39 +0900

    [Passed] 정사각형 두 개 출력 (91ms, 9MB)

---
+++ trail1/정사각형 두 개 출력/output-two-rectangles.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] lines = br.readLine().split(" ");
+
+        int N = Integer.parseInt(lines[0]);
+
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < N; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < N; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
