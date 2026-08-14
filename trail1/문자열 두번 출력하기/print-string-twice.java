02579950dd35d1f619a25a49158f8409870192d3
Nuyo <lgy000405@gmail.com>
2026-08-14 20:18:26 +0900

    [Passed] 문자열 두번 출력하기 (126ms, 10MB)

---
+++ trail1/문자열 두번 출력하기/print-string-twice.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String n = sc.next();
+
+        System.out.println(n);
+        System.out.println(n);
+    }
+}
