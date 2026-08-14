339e71b241a13b69ab3696866daecee797898a70
Nuyo <lgy000405@gmail.com>
2026-08-15 08:49:14 +0900

    [Passed] 공백을 기준으로 출력 (128ms, 10MB)

---
+++ trail1/공백을 기준으로 출력/output-based-on-space.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+
+        System.out.print(a.replace(" ",""));
+        System.out.print(b.replace(" ",""));
+    }
+}
