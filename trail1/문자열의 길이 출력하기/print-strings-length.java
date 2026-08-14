0ddab82aa95f1b1ad8830690025a70189f9beb88
Nuyo <lgy000405@gmail.com>
2026-08-14 20:20:05 +0900

    [Passed] 문자열의 길이 출력하기 (128ms, 10MB)

---
+++ trail1/문자열의 길이 출력하기/print-strings-length.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String first = sc.next();
+        String second = sc.next();
+
+        System.out.print(first.length() + second.length());
+    }
+}
