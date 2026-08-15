9408e62b41525488beb8d0b87d02784eb8f6a228
Nuyo <lgy000405@gmail.com>
2026-08-15 13:14:47 +0900

    [Passed] 문자열에서 문자 출력 (122ms, 10MB)

---
+++ trail1/문자열에서 문자 출력/print-chars-from-word.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+
+        for (int i = 0; i < s.length(); i++){
+            System.out.println(s.charAt(i));
+        }
+    }
+}
