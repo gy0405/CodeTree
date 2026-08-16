211f0c51579aaea84959de041e767bca8fe45406
Nuyo <lgy000405@gmail.com>
2026-08-16 19:43:14 +0900

    [Passed] 두 문자열을 이어붙였을 때 (124ms, 10MB)

---
+++ trail1/두 문자열을 이어붙였을 때/when-two-strings-are-concatenated.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+
+        if ((a+b).equals(b+a)){
+            System.out.println("true");
+        }
+        else
+            System.out.println("false");
+    }
+}
