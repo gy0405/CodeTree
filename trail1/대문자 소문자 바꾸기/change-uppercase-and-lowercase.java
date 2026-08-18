bafdcaee22971011b3e3a522a9f24d8bdd7d55a9
Nuyo <lgy000405@gmail.com>
2026-08-18 19:05:39 +0900

    [Passed] 대문자 소문자 바꾸기 (128ms, 10MB)

---
+++ trail1/대문자 소문자 바꾸기/change-uppercase-and-lowercase.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        for(int i = 0; i < str.length(); i++){
+            if ( 'A' <=  str.charAt(i) && str.charAt(i) <= 'Z'){
+                System.out.print((char) (str.charAt(i) + 32));
+            }
+            else if ('a' <=  str.charAt(i) && str.charAt(i) <= 'z'){
+                System.out.print((char) (str.charAt(i) - 32));
+
+            }
+
+        }
+    }
+}
