6b47d29c7ea5cd976115ef1d2dbd205fda140a33
Nuyo <lgy000405@gmail.com>
2026-08-18 18:50:25 +0900

    [Passed] 아스키코드 변환 (141ms, 12MB)

---
+++ trail1/아스키코드 변환/convert-to-ascii.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        char a = sc.next().charAt(0);
+        int b = sc.nextInt();
+
+        System.out.printf("%d %c", (int) a, (char) b);
+    }
+}
