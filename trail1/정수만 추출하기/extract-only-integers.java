5405e3d6a528b3a718ec6ad382dc6031e41501c7
Nuyo <lgy000405@gmail.com>
2026-08-19 18:52:39 +0900

    [Passed] 정수만 추출하기 (132ms, 10MB)

---
+++ trail1/정수만 추출하기/extract-only-integers.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        String b = sc.next();
+
+        String inta = "";
+        String intb = "";
+
+        for (int i = 0; i < a.length(); i++){
+            if ( '0' <= a.charAt(i) && a.charAt(i) <= '9'){
+                inta += a.charAt(i);
+            }
+            else{
+                break;
+            }
+        }
+
+        for (int i = 0; i < b.length(); i++){
+            if ( '0' <= b.charAt(i) && b.charAt(i) <= '9'){
+                intb += b.charAt(i);
+            }
+            else{
+                break;
+            }
+        }
+
+        int answer = Integer.parseInt(inta) + Integer.parseInt(intb);
+
+        System.out.println(answer);
+    }
+}
