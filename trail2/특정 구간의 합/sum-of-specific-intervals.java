4b35a90bac4d3fe9faa50d81edc45cc92617da75
Nuyo <lgy000405@gmail.com>
2026-08-21 21:57:25 +0900

    [Passed] 특정 구간의 합 (138ms, 10MB)

---
+++ trail2/특정 구간의 합/sum-of-specific-intervals.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main {
+
+    public static int[] arr = new int[100];
+
+    public static void printSum(int a1, int a2){
+        int sum = 0;
+        for (int i = a1 - 1; i < a2; i++){
+            sum += arr[i];
+        }
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < m; i++) {
+            int a1 = sc.nextInt();
+            int a2 = sc.nextInt();
+
+            printSum(a1, a2);
+        }
+    }
+}
