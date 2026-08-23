7a6fd2c7bdf6807fd5997da36f4a64268f4d71c3
Nuyo <lgy000405@gmail.com>
2026-08-23 18:12:32 +0900

    [Passed] 큰 수 자리수의 합 (124ms, 10MB)

---
+++ trail2/큰 수 자리수의 합/sum-of-large-numeric-digits.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static int f(int n){
+        if (n < 10){
+            return n;
+        }
+
+        return f(n / 10) + (n % 10);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        // Please write your code here.
+        System.out.println(f(a*b*c));
+    }
+}
