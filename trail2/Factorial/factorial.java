d1f1695023159751bf2e76423d9009350b78d4b9
Nuyo <lgy000405@gmail.com>
2026-08-23 18:04:32 +0900

    [Passed] Factorial (129ms, 10MB)

---
+++ trail2/Factorial/factorial.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int fac(int n){
+        if (n == 0 || n == 1){
+            return 1;
+        }
+
+        return fac(n-1) * n;
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+        System.out.println(fac(n));
+    }
+}
