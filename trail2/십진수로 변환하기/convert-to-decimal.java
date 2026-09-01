93aaacc7bfcf652c3f37b5e282e9f3e3957d8599
Nuyo <lgy000405@gmail.com>
2026-09-01 17:48:32 +0900

    [Passed] 십진수로 변환하기 (122ms, 10MB)

---
+++ trail2/십진수로 변환하기/convert-to-decimal.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String binary = sc.next();
+        // Please write your code here.
+        char[] arr = binary.toCharArray();
+
+        int answer = 0;
+
+        for(int i = 0; i <arr.length; i++){
+            answer = answer * 2 + (arr[i] - '0');
+        }
+
+        System.out.print(answer);
+    }
+}
