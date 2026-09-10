ff0d56fd7e183472d225b0958f0fa47888200258
Nuyo <lgy000405@gmail.com>
2026-09-10 21:22:35 +0900

    [Passed] 연속되는 수 3 (183ms, 12MB)

---
+++ trail2/연속되는 수 3/continuous-number3.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        
+        // Please write your code here.
+
+        int max = 0;
+        int cnt = 1;
+
+        for(int i = 0; i < arr.length; i++){
+            if(i == 0){
+                continue;
+            } 
+            else if(arr[i] * arr[i-1] > 0){
+                cnt++;
+            }
+            else{
+                if (max < cnt){
+                    max = cnt;
+                }
+                cnt = 1;
+            }
+        }
+
+        if (max < cnt){
+            max = cnt;
+        }
+
+        System.out.println(max);
+    }
+}
